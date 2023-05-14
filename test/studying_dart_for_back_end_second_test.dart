e0e2c8cd5a4f51a5a819ea0ee9655892005f77e0
Felipe Bellini <felipe.bellini@krykto.com>
2023-05-14 16:26:53 -0300

    push project

---
+++ test/studying_dart_for_back_end_second_test.dart
@@ -0,0 +1,8 @@
+import 'package:studying_dart_for_back_end_second/studying_dart_for_back_end_second.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
